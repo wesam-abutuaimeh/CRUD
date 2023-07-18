df1150be356f8ca26bb2e56b59c0a351a8f8dc47
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-18 13:27:23 +0300

    Make code cleaner

--- src/WithParams/index.jsx
+++ src/WithParams/index.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
 
-// HOC => Higher Order Component
 const WithParams = (WrappedComponent) => {
     return (props) => {
         // eslint-disable-next-line react-hooks/rules-of-hooks
