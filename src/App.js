77de46d0d34fae0899f2d835bd13e2048f6acc9d
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-19 22:27:36 +0300

    Using Paths as constans for organization project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Route, Routes } from "react-router-dom";
+import Router from "./routers";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import Stores from "./pages/Stores";
 import Store from "./pages/Store";
 import Create from "./pages/Create";
+import Edit from "./pages/Edit";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path={"/"} element={<HomePage />} />
-        <Route path="stores/all" element={<Stores />} />
-        <Route path="stores/:id" element={<Store />} />
-        <Route path="create" element={<Create />} />
+        <Route path={Router.Home} element={<HomePage />} />
+        <Route path={Router.CRUD.STORES} element={<Stores />} />
+        <Route path={Router.CRUD.STORE} element={<Store />} />
+        <Route path={Router.CRUD.CREATE} element={<Create />} />
+        <Route path={Router.CRUD.EDIT} element={<Edit />} />
       </Routes>
     </div>
   );
