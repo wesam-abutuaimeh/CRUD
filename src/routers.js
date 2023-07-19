60b4027ffeb7e8c251ba8d18d6f38718254f8f2f
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-19 22:28:10 +0300

    Doing Paths in  separated  file

---
+++ src/routers.js
@@ -0,0 +1,10 @@
+const PATHS = {
+  HOME: "/",
+  CRUD: {
+    STORES: "/stores/all",
+    STORE: "/stores/:id",
+    CREATE: "/create",
+    EDIT: "/stores/:id/edit",
+  },
+};
+export default PATHS;
