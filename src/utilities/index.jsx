a3908b33814e3d0e572a1c5a63c1db212bc6051f
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-07-25 12:51:17 +0300

    Make code more clean

--- src/utilities/index.jsx
+++ src/utilities/index.jsx
@@ -1,34 +1,17 @@
 import axios from "axios";
 
 const apiCalls = async (variant, url, requestData) => {
+    const requests = {
+        'get': async () => await axios.get(url),
+        'create': async () => await axios.post(url, requestData),
+        'update': async () => await axios.put(url, requestData),
+        'delete': async () => await axios.delete(url),
+    }
     try {
-        let response;
-        switch (variant) {
-            case "get":
-                response = await axios.get(url);
-                break;
-            case "create":
-                response = await axios.post(url, requestData);
-                break;
-            case "update":
-                response = await axios.put(url, requestData);
-                break;
-            case "delete":
-                response = await axios.delete(url);
-                break;
-            default:
-                throw new Error("Invalid variant provided.");
-        }
-        if (!response.data) {
-            throw new Error("No data received from the server.");
-        }
+        const response = await requests[variant]();
         return response.data;
-    } catch (error) {
-        if (error.response && error.response.data && error.response.data.message) {
-            throw new Error(error.response.data.message);
-        } else {
-            throw new Error("Error while making the API call!");
-        }
+    } catch {
+        throw new Error("API call error !");
     }
 };
 
