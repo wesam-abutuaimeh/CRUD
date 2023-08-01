5891ee013f5e8971b96a0dbdabfc1f472de4b1ea
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-01 11:27:48 +0300

    Use useReducer Hook

--- src/hooks/useAPI.jsx
+++ src/hooks/useAPI.jsx
@@ -1,70 +1,92 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import axios from "axios";
 
+const initalState = {
+    data: [],
+    isLoading: false,
+    error: null,
+};
+
+const ACTIONS_TYPE = {
+    FETCH__START: "FETCH__START",
+    FETCH__SUCCESSFUL: "FETCH__SUCCESSFUL",
+    FETCH__ERROR: "FETCH__ERROR",
+    POST: "POST",
+    DELETE: "DELETE",
+    PUT: "PUT",
+};
+
+const apiReducer = (state, action) => {
+    switch (action.type) {
+        case ACTIONS_TYPE.FETCH__START:
+            return { ...state, isLoading: true };
+        case ACTIONS_TYPE.FETCH__SUCCESSFUL:
+            return { ...state, data: action.payload, isLoading: false };
+        case ACTIONS_TYPE.FETCH__ERROR:
+            return { ...state, isLoading: false, error: action.payload };
+        case ACTIONS_TYPE.POST:
+            return { ...state, data: [...state.data, action.payload], isLoading: false, };
+        case ACTIONS_TYPE.DELETE:
+            return { ...state, data: state.data.filter((item) => item.id !== action.payload), isLoading: false, };
+        case ACTIONS_TYPE.PUT:
+            return { ...state, data: action.payload };
+        default:
+            throw Error(`Action Type Error! ${action.type}`);
+    }
+};
+
 const useAPI = (API_URL) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+    const [state, dispatch] = useReducer(apiReducer, initalState);
     const get = async () => {
         try {
-            setIsLoading(true);
+            dispatch({ type: ACTIONS_TYPE.FETCH__START });
             const response = await axios.get(API_URL);
-            setData(response?.data?.data || response?.data);
+            dispatch({ type: ACTIONS_TYPE.FETCH__SUCCESSFUL, payload: response?.data?.data || response?.data });
         } catch (error) {
-            throw new Error(error);
-        } finally {
-            setIsLoading(false);
+            dispatch({ type: ACTIONS_TYPE.FETCH__ERROR, payload: Error(error) });
         }
     };
-
     const getItem = async (id) => {
         try {
+            dispatch({ type: ACTIONS_TYPE.FETCH__START });
             const response = await axios.get(`${API_URL}/${id}`);
-            setData(response?.data?.data || response?.data);
-            setIsLoading(true);
+            dispatch({ type: ACTIONS_TYPE.FETCH__SUCCESSFUL, payload: response?.data?.data || response?.data });
         } catch (error) {
-            throw new Error(error);
-        } finally {
-            setIsLoading(false);
+            dispatch({ type: ACTIONS_TYPE.FETCH__ERROR, payload: Error(error) });
         }
     };
-
     const deleteItem = async (id) => {
         try {
             await axios.delete(`${API_URL}/${id}`);
-            setData((prevState) => prevState.filter((itemId) => itemId !== id));
-            setIsLoading(true);
+            dispatch({ type: ACTIONS_TYPE.DELETE });
         } catch (error) {
-            throw new Error(error);
-        } finally {
-            setIsLoading(false);
+            dispatch({ type: ACTIONS_TYPE.FETCH__ERROR, payload: Error(error) });
         }
     };
-
     const post = async (body) => {
         try {
-            setIsLoading(true);
+            dispatch({ type: ACTIONS_TYPE.FETCH__START });
             const response = await axios.post(`${API_URL}`, body);
-            setData((prevState) => [...prevState, response.data.data]);
+            dispatch({
+                type: ACTIONS_TYPE.POST, payload: (prevState) => [...prevState, response.data.data]
+            });
         } catch (error) {
-            throw new Error(error);
-        } finally {
-            setIsLoading(false); // Set loading state to false after the API call is complete
+            dispatch({ type: ACTIONS_TYPE.FETCH__ERROR, payload: Error(error) });
         }
     };
-
     const put = async (url, data) => {
         try {
             const response = await axios.put(url, data);
-            setIsLoading(true);
-            return response.data;
+            dispatch({ type: ACTIONS_TYPE.PUT, payload: response.data });
         } catch (error) {
-            throw error;
-        } finally {
-            setIsLoading(false);
+            dispatch({ type: ACTIONS_TYPE.FETCH__ERROR, payload: Error(error) });
         }
-    }
-
-    return { data, isLoading, get, getItem, deleteItem, post, put };
-}
+    };
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        get, getItem, deleteItem, post, put,
+    };
+};
 
 export default useAPI;
